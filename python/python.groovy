12d330c8e186febd21e7e83cd1ebde35a9d8b8d0
Dolev-Tz <dolevtzviel123@gmail.com>
2024-11-03 17:18:56 +0200

    Create python.groovy

---
+++ python/python.groovy
@@ -0,0 +1,44 @@
+pipelineJob("frestyle_python") {
+
+  definition {
+           cps {
+             script('''
+             def image
+                 pipeline {
+                    agent any
+
+                    environment {
+                        DOCKERHUB_CREDENTIALS = credentials('dockerhub-credentials-id')
+                        DOCKER_IMAGE = 'dolevtzvieltovim/flask-docker-app'
+                    }
+                    stages {
+                        stage('Checkout Code') {
+                            steps {
+                                git url: 'https://github.com/Dolev-Tz/Home-assignment.git', branch: 'python'
+                            }
+                        }
+
+                        stage('Build Docker Image') {
+                            steps {
+                                script {
+                                    image = docker.build(env.DOCKER_IMAGE)
+                                }
+                            }
+                        }
+
+                        stage('Push to Docker Hub') {
+                            steps {
+                                script {
+                                    docker.withRegistry('https://index.docker.io/v1/', DOCKERHUB_CREDENTIALS) {
+                                        image.push('latest')
+                                    }
+                                }
+                            }
+                        }
+                      }
+                   }
+              '''.stripIndent())
+       sandbox()
+          }
+      }
+  }
