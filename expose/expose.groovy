4b7103f1a64c37fb143bd5cc1e112f3b4cf72405
Dolev-Tz <dolevtzviel123@gmail.com>
2024-11-06 11:55:03 +0200

    Update expose.groovy

--- expose/expose.groovy
+++ expose/expose.groovy
@@ -16,9 +16,9 @@ pipelineJob('run-nginx-flask-job') {
                     stage('Run Containers') {
                         steps {
                             script {
-                                echo 'Running Flask and Nginx containers...'
+                                echo 'Running Flask and Nginx containers'
 
-                                echo 'Docker network create app_network'
+                                bat 'Docker network create app_network'
                                 // Start Flask app container
                                 bat "docker run -d -p 5000:5000 --network app_network --name flask_app_expose1 -v /var/run/docker.sock:/var/run/docker.sock ${FLASK_IMAGE}"
                                 
@@ -31,32 +31,18 @@ pipelineJob('run-nginx-flask-job') {
                     stage('Verify Setup') {
                         steps {
                             script {
-                                echo 'Verifying that Nginx is proxying requests correctly...'
+                                echo 'Verifying that Nginx is proxying requests correctly'
 
                                 def curlCommand = "curl -s -o NUL http://localhost:${LOCAL_PORT}"
                                 
-                                echo "${curlCommand}"
                                 def responseCode = bat(script: "echo %ERRORLEVEL%", returnStdout: true).trim()
-                                echo "${responseCode}"
 
                                 // Check the HTTP response code
-                                if (responseCode == '200') {
+                                if (responseCode == '0') {
                                     echo 'Request successful! Nginx is functioning correctly.'
                                 } else {
                                     error "Request failed with HTTP status code: ${responseCode}"
                                 }
-                            }
-                        }
-                    }
-
-                    stage('Cleanup') {
-                        steps {
-                            script {
-                                echo 'Stopping and removing containers...'
-                                bat "docker stop flask_app || true"
-                                bat "docker rm flask_app || true"
-                                bat "docker stop nginx_proxy || true"
-                                bat "docker rm nginx_proxy || true"
                             }
                         }
                     }
